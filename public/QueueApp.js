3ca8e66614070a24e1513d1e2aa284b7b7d0ac3e
Dan Govan <daniel.govan@gmail.com>
2020-08-03 17:11:47 +0100

    Start point

--- public/QueueApp.js
+++ public/QueueApp.js
@@ -1,47 +1,44 @@
 (function () {
+  angular
+    .module("qudini.QueueApp", [])
+    .controller("QueueController", QueueController);
 
-	angular.module('qudini.QueueApp', [])
-		.controller('QueueController', QueueController);
+  QueueController.$inject = ["$scope", "$http"];
 
-	QueueController.$inject = ['$scope', '$http'];
+  /**
+   * Bonus points - manipulating the without waiting for the
+   * server request
+   */
+  function QueueController($scope, $http) {
+    $scope.customers = [];
+    $scope.customersServed = [];
 
-	/**
-	* Bonus points - manipulating the without waiting for the
-	* server request
-	*/
-	function QueueController($scope, $http) {
+    _getCustomers();
+    _getServedCustomers();
 
-		$scope.customers = [];
-		$scope.customersServed = [];
+    $scope.onCustomerAdded = function () {
+      _getCustomers();
+    };
 
-		_getCustomers();
-		_getServedCustomers();
+    $scope.onCustomerRemoved = function () {
+      _getCustomers();
+    };
 
-		$scope.onCustomerAdded = function () {
-			_getCustomers();
-		};
+    $scope.onCustomerServed = function () {
+      _getCustomers();
+      _getServedCustomers();
+    };
 
-		$scope.onCustomerRemoved = function () {
-			_getCustomers();
-		};
+    function _getServedCustomers() {
+      return $http.get("/api/customers/served").then(function (res) {
+        $scope.customersServed = res.data;
+      });
+    }
 
-		$scope.onCustomerServed = function () {
-			_getCustomers();
-			_getServedCustomers();
-		};
-
-		function _getServedCustomers() {
-			return $http.get('/api/customers/served').then(function (res) {
-				$scope.customersServed = res.data;
-			});
-		}
-
-		function _getCustomers() {
-			return $http.get('/api/customers').then(function (res) {
-				$scope.customers = res.data;
-			});
-		}
-
-	}
-
+    function _getCustomers() {
+      return $http.get("/api/customers").then(function (res) {
+        $scope.customers = res.data;
+      });
+    }
+  }
 })();
